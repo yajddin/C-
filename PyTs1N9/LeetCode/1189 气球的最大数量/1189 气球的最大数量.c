10ef0b558f1b2bd6b009a071cfa19aa1e1ee7a8d
PyTs1n9 <1990629881@qq.com>
2026-07-11 01:33:33 +0800

    commit 02 气球的最大数量

--- PyTs1N9/LeetCode/1189 气球的最大数量/1189 气球的最大数量.c
+++ PyTs1N9/LeetCode/1189 气球的最大数量/1189 气球的最大数量.c
@@ -1,7 +1,30 @@
 // 先统计text每个字母出现的次数，然后每种字母最多能支持几个balloon
 #include  <stdio.h>
 
+int min(int x, int y) {
+    return x < y ? x : y;
+}
+
+int maxNumberOfBalloons(char *text) {
+    int count[26] = {0};
+
+    for (int i = 0; text[i] != '\0'; i++) {
+        count[text[i] - 'a']++;
+    }
+
+    int b = count['b' - 'a'];
+    int a = count['a' - 'a'];
+    int l = count['l' - 'a'] / 2;
+    int o = count['o' - 'a'] / 2;
+    int n = count['n' - 'a'];
+
+    return min(min(min(min(b, a), l), o), n);
+}
+
+
 int main() {
-    
+    printf("%d\n", maxNumberOfBalloons("nlaebolko"));
+    printf("%d\n", maxNumberOfBalloons("loonbalxballpoon"));
+    printf("%d\n", maxNumberOfBalloons("leetcode"));
     return 0;
 }
