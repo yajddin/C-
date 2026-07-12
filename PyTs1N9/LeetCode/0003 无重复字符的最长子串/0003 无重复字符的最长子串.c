3f33e13cc389f55c8c595aec96077dcff03073c4
PyTs1n9 <1990629881@qq.com>
2026-07-12 12:18:31 +0800

    commit 暴力枚举 无重复字符的最长子串

--- PyTs1N9/LeetCode/0003 无重复字符的最长子串/0003 无重复字符的最长子串.c
+++ PyTs1N9/LeetCode/0003 无重复字符的最长子串/0003 无重复字符的最长子串.c
@@ -1,8 +1,41 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+bool contains(char arr[], int size, char value) {
+    for (int i = 0; i < size; i++) {
+        if (arr[i] == value) {
+            return true;
+        }
+    }
+    return false;
+}
+
+int lengthOfLongestSubstring(char *s) {
+    int max_len = 0;
+    int size = strlen(s);
+
+    for (int i = 0; i < size; i++) {
+        char arr[size];
+        int arr_size = 0;
+
+        for (int j = i; j < size; j++) {
+            if (contains(arr, arr_size, s[j])) {
+                break;
+            }
+            arr[arr_size] = s[j];
+            arr_size++;
+
+            if (arr_size > max_len) {
+                max_len = arr_size;
+            }
+        }
+    }
+    return max_len;
+}
 
 int main() {
-
-
+    printf("%d\n", lengthOfLongestSubstring("abcabcbb"));
+    printf("%d\n", lengthOfLongestSubstring("pwwkew"));
     return 0;
-
 }
